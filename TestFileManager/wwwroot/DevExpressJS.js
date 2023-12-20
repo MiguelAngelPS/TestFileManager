81efe216b292255302f952fcbe98755580f2df23
Miguel Angel <Miguel Angel@PC-GPMA>
2023-12-20 13:08:14 +0100

    Agregar archivos de proyecto.

---
+++ TestFileManager/wwwroot/DevExpressJS.js
@@ -0,0 +1,44 @@
+﻿function renderDevExtremeControls(filemanagerContainerID) {
+    renderFileManager(filemanagerContainerID);
+}
+
+function renderFileManager(container) {
+
+    const provider = new DevExpress.fileManagement.RemoteFileSystemProvider({
+        endpointUrl: 'http://localhost:46357/api/file-manager-file-system-images',
+        //endpointUrl: 'https://js.devexpress.com/Demos/Mvc/api/file-manager-file-system-images',
+    });
+
+    $('#file-manager').dxFileManager({
+        name: 'fileManager',
+        fileSystemProvider: provider,
+        currentPath: 'Widescreen',
+        permissions: {
+            create: true,
+            copy: true,
+            move: true,
+            delete: true,
+            rename: true,
+            upload: true,
+            download: true,
+        },
+        onSelectedFileOpened(e) {
+            const popup = $('#photo-popup').dxPopup('instance');
+            popup.option({
+                title: e.file.name,
+                contentTemplate: `<img src="${e.file.dataItem.url}" class="photo-popup-image" />`,
+            });
+            popup.show();
+        },
+    });
+
+    $('#photo-popup').dxPopup({
+        maxHeight: 600,
+        hideOnOutsideClick: true,
+        showCloseButton: true,
+        onContentReady(e) {
+            const $contentElement = e.component.content();
+            $contentElement.addClass('photo-popup-content');
+        },
+    });
+}
